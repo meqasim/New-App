92b05a4489b0d1756c41ff5597536aa02196948f
HaFiZ CH QaSiM <63259297+meqasim@users.noreply.github.com>
2024-06-02 03:21:25 +0500

    Site full funtional with with API and URL is working

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = 'c24fad1fc29047f88d199045c3fe272b';
 const url = 'https://newsapi.org/v2/everything?q=';
 
-window.addEventListener('load', () => fetchNews('tesla'));
+window.addEventListener('load', () => fetchNews('Pakistan'));
 
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
@@ -11,7 +11,38 @@ async function fetchNews(query) {
 
 function bindData(articles) {
     const cardContainer = document.getElementById('cards-container');
-    const newsCardTemplate = document, getElementById('newsCardTemplate');
+    const newsCardTemplate = document.getElementById('news-card-template');
 
     cardContainer.innerHTML = '';
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+
+        fillDataInCard(cardClone, article)
+        cardContainer.appendChild(cardClone);
+        console.log(article);
+    });
+}
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#card-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleTimeString("en-US", {
+        timeZone: "Asia/Karachi",
+    });
+
+    newsSource.innerHTML = '${article.source.name} . ${date}';
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    })
 }
